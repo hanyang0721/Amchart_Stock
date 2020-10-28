a527298eba6ecd04af0d62b1791a7c4cc7824c8e
hanyang0721 <hanyang0721@gmail.com>
2020-10-28 16:55:17 +0800

    First release

---
+++ Controllers/SampleChartController.cs
@@ -0,0 +1,42 @@
+﻿using SampleChart.DataModel;
+using SampleChart.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleChart.Controllers
+{
+    public class SampleChartController : Controller
+    {
+        //create instance of datacontext
+        SampleDbEntities db = new SampleDbEntities();
+
+        // GET: SampleChart
+        public ActionResult AmChartSample()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult GetChartData()
+        {
+            var model = db.Prices
+                .ToList()
+                .Select(m => new ChartDataDTO
+                 {
+                    Id=m.ID,
+                    nopen=m.nopen.HasValue?m.nopen.Value:0,
+                    nhigh= m.nhigh.HasValue ? m.nhigh.Value : 0,
+                    nlow = m.nlow.HasValue ? m.nlow.Value : 0,
+                    nclose = m.nclose.HasValue ? m.nclose.Value : 0,
+                    nvol = m.nvol.HasValue ? m.nvol.Value : 0,
+                    Date =m.Date.Value.ToString("yyyy-MM-dd")
+                }).OrderBy(j=>j.Date).ToList();
+
+            return Json(model.ToArray(), JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ Models/ChartDataDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SampleChart.Models
+{
+    public class ChartDataDTO
+    {
+        public int Id { get; set; }
+        public string Date { get; set; }
+        public int nopen { get; set; }
+        public int nhigh { get; set; }
+        public int nlow { get; set; }
+        public int nclose { get; set; }
+        public int nvol { get; set; }
+
+    }
+}
